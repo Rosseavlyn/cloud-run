00148b573e2d1e070e06e2249b8506b3dcbbebce
Rosseavlyn <95288447+Rosseavlyn@users.noreply.github.com>
2023-03-14 08:47:04 +0700

    arabic changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send('تمر بهذه الحياة لأول مرة');
 });
 
 const PORT = process.env.PORT || 8080;
